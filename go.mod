524bcb093246e65d7f45170df43dcbc88d1c4b38
Farry <newfarry@126.com>
2020-05-11 13:59:03 +0800

    rename project; remove server grpc api

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/OpenIoTHub/grpc-api
+module github.com/OpenIoTHub/openiothub-grpc-api
 
 go 1.14
 
--- lib/pb/service.pbjson.dart
+++ lib/pb/service.pbjson.dart
@@ -126,28 +126,6 @@ const SOCKS5List$json = const {
   ],
 };
 
-const HTTPConfig$json = const {
-  '1': 'HTTPConfig',
-  '2': const [
-    const {'1': 'RunId', '3': 1, '4': 1, '5': 9, '10': 'RunId'},
-    const {'1': 'Domain', '3': 2, '4': 1, '5': 9, '10': 'Domain'},
-    const {'1': 'RemoteIP', '3': 3, '4': 1, '5': 9, '10': 'RemoteIP'},
-    const {'1': 'RemotePort', '3': 4, '4': 1, '5': 5, '10': 'RemotePort'},
-    const {'1': 'UserName', '3': 5, '4': 1, '5': 9, '10': 'UserName'},
-    const {'1': 'Password', '3': 6, '4': 1, '5': 9, '10': 'Password'},
-    const {'1': 'IfHttps', '3': 7, '4': 1, '5': 8, '10': 'IfHttps'},
-    const {'1': 'Description', '3': 8, '4': 1, '5': 9, '10': 'Description'},
-    const {'1': 'RemotePortStatus', '3': 9, '4': 1, '5': 8, '10': 'RemotePortStatus'},
-  ],
-};
-
-const HTTPList$json = const {
-  '1': 'HTTPList',
-  '2': const [
-    const {'1': 'HTTPConfigs', '3': 1, '4': 3, '5': 11, '6': '.pb.HTTPConfig', '10': 'HTTPConfigs'},
-  ],
-};
-
 const MDNSServiceList$json = const {
   '1': 'MDNSServiceList',
   '2': const [
