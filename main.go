a830c6705e221386c8f0fe653a5c613cc8cce71a
iotserv <yu@iotserv.com>
2020-10-27 18:08:49 +0800

    update

--- main.go
+++ main.go
@@ -1,3 +1,3 @@
-//go:generate protoc --go-grpc_out=./pb-go service.proto
+//go:generate protoc --go_out=./pb-go --go_opt=paths=source_relative  --go-grpc_out=./pb-go --go-grpc_opt=paths=source_relative service.proto
 //go:generate protoc --dart_out=grpc:./lib/pb service.proto
 package grpc_api
